7d94fe3a47d70f95e2fd3910c4f06364767b0c41
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2021-05-12 03:34:44 +0200

    Update local paths

--- config/lineage_audio.mk
+++ config/lineage_audio.mk
@@ -2,9 +2,9 @@
 # Lineage Audio Files
 #
 
-ALARM_PATH := vendor/lineage/prebuilt/common/media/audio/alarms
-NOTIFICATION_PATH := vendor/lineage/prebuilt/common/media/audio/notifications
-RINGTONE_PATH := vendor/lineage/prebuilt/common/media/audio/ringtones
+ALARM_PATH := vendor/lineagepixel/prebuilt/common/media/audio/alarms
+NOTIFICATION_PATH := vendor/lineagepixel/prebuilt/common/media/audio/notifications
+RINGTONE_PATH := vendor/lineagepixel/prebuilt/common/media/audio/ringtones
 
 # Alarms
 PRODUCT_COPY_FILES += \
